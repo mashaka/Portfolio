8dd086aba23b630433913849c04820ffe952b148
Sandrikova Maria <mashasandrikova@gmail.com>
2018-03-15 23:24:33 +0000

    Fix errors that broke config

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { MdButton, MdCard, MdChip } from 'vue-material/dist/components'
+import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-awesome/icons'
 import App from './components/App.vue'
@@ -11,9 +11,7 @@ import Icon from 'vue-awesome/components/Icon'
 import './assets/img/favicon.ico'
 
 Vue.component('icon', Icon)
-Vue.use(MdButton)
-Vue.use(MdCard)
-Vue.use(MdChip)
+Vue.use(VueMaterial)
 Vue.use(VueScrollTo)
 Vue.use(BootstrapVue)
 
